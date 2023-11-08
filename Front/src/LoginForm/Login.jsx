c882a4801ae61efe951562a51f90e41cc8f72657
kevin glaentzlin <kevin@mbp-de-kevin.home>
2023-11-08 22:16:21 +0100

    [+] Add mobile version for login form

--- Front/src/LoginForm/Login.jsx
+++ Front/src/LoginForm/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Container, TextField, Button, InputAdornment } from "@mui/material";
+import { Box, Container, TextField, Button, InputAdornment, useMediaQuery } from "@mui/material";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
@@ -16,78 +16,32 @@ export default function Login() {
         setShowPassword(!showPassword);
     };
 
-    return (
-        <Box
-            sx={{
-                height: '100vh',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-                position: 'relative',
-            }}
-        >
-            <img
-                src={"https://source.unsplash.com/random"}
-                alt={"background"}
-                style={{
-                    filter: 'brightness(50%)',
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                    width: '100%',
-                    maxHeight: '100vh',
-                    objectFit: 'cover',
-                    position: 'absolute',
-                    objectPosition: 'center',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    zIndex: -1,
-                }}
-            />
-            <Container
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-                <Box
-                    sx={{
-                        height: '60vh',
-                        width: '60vh',
-                        backgroundColor: 'white',
-                        borderRadius: 4,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        alignContent: 'center',
-                    }}
-                >
-                    <h1 style={{
-                        marginBottom: showRegistration ? '2rem' : '5rem',
-                        fontSize: '2rem'
-                    }}>
-                        {showRegistration ? 'Inscription' : 'Connexion'}
-                    </h1>
+    const isMobile = useMediaQuery('(max-width:600px)');
 
+    return (
+        <>
+            {isMobile ? (
+//-----------------------------------------------------VERSION MOBILE
+                <Container>
                     <Box
-                        component="form"
                         sx={{
                             display: 'flex',
                             flexDirection: 'column',
                             alignItems: 'center',
-                            justifyContent: 'center',
                         }}
                     >
+                        <h1 style={{
+                            marginBottom: showRegistration ? '2rem' : '5rem',
+                            fontSize: '2rem'
+                        }}>
+                            {showRegistration ? 'Inscription' : 'Connexion'}
+                        </h1>
+
                         {showRegistration && (
                             <TextField
                                 sx={{
-                                    width: '55vh',
+                                    width: '100%',
                                     marginBottom: '1rem',
-                                    justifyContent: 'center',
                                 }}
                                 label="Adresse-mail"
                                 InputProps={{
@@ -101,7 +55,7 @@ export default function Login() {
                         )}
                         <TextField
                             sx={{
-                                width: '55vh',
+                                width: '100%',
                                 marginBottom: '1rem',
                             }}
                             label="Nom d'utilisateur"
@@ -115,7 +69,7 @@ export default function Login() {
                         />
                         <TextField
                             sx={{
-                                width: '55vh',
+                                width: '100%',
                                 marginBottom: '1rem',
                             }}
                             label="Mot de passe"
@@ -136,8 +90,8 @@ export default function Login() {
                                 ),
                             }}
                         />
-                        <Button variant="contained" type="submit" sx={{ width: '20vh' }}>
-                            {showRegistration ? 'S\'enregistrer' : 'Se connecter'}
+                        <Button variant="contained" type="submit" sx={{ width: '100%' }}>
+                            {showRegistration ? "S'enregistrer" : 'Se connecter'}
                         </Button>
                         <Button
                             variant="text"
@@ -149,9 +103,145 @@ export default function Login() {
                                 : 'Pas encore membre ? Inscrivez-vous ici'}
                         </Button>
                     </Box>
-                </Box>
-            </Container>
-        </Box>
-    );
-}
+                </Container>
+            ) : (
+//--------------------------------------------------------------- VERSION PC
+                <Box
+                    sx={{
+                        height: '100vh',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        flexDirection: 'column',
+                        position: 'relative',
+                    }}
+                >
+                    <img
+                        src={"https://source.unsplash.com/random"}
+                        alt={"background"}
+                        style={{
+                            filter: 'brightness(50%)',
+                            backgroundPosition: 'center',
+                            backgroundSize: 'cover',
+                            width: '100%',
+                            maxHeight: '100vh',
+                            objectFit: 'cover',
+                            position: 'absolute',
+                            objectPosition: 'center',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            bottom: 0,
+                            zIndex: -1,
+                        }}
+                    />
+                    <Container
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                height: '60vh',
+                                width: '60vh',
+                                backgroundColor: 'white',
+                                borderRadius: 4,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                alignContent: 'center',
+                            }}
+                        >
+                            <h1 style={{
+                                marginBottom: showRegistration ? '2rem' : '5rem',
+                                fontSize: '2rem'
+                            }}>
+                                {showRegistration ? 'Inscription' : 'Connexion'}
+                            </h1>
 
+                            <Box
+                                component="form"
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                {showRegistration && (
+                                    <TextField
+                                        sx={{
+                                            width: '55vh',
+                                            marginBottom: '1rem',
+                                            justifyContent: 'center',
+                                        }}
+                                        label="Adresse-mail"
+                                        InputProps={{
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    <AccountCircleIcon />
+                                                </InputAdornment>
+                                            ),
+                                        }}
+                                    />
+                                )}
+                                <TextField
+                                    sx={{
+                                        width: '55vh',
+                                        marginBottom: '1rem',
+                                    }}
+                                    label="Nom d'utilisateur"
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <AccountCircleIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                                <TextField
+                                    sx={{
+                                        width: '55vh',
+                                        marginBottom: '1rem',
+                                    }}
+                                    label="Mot de passe"
+                                    type={showPassword ? 'text' : 'password'}
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                {showPassword ? (
+                                                    <VisibilityIcon
+                                                        onClick={togglePasswordVisibility}
+                                                    />
+                                                ) : (
+                                                    <VisibilityOffIcon
+                                                        onClick={togglePasswordVisibility}
+                                                    />
+                                                )}
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                />
+                                <Button variant="contained" type="submit" sx={{ width: '20vh' }}>
+                                    {showRegistration ? 'S\'enregistrer' : 'Se connecter'}
+                                </Button>
+                                <Button
+                                    variant="text"
+                                    sx={{ marginTop: '1rem' }}
+                                    onClick={toggleRegistration}
+                                >
+                                    {showRegistration
+                                        ? 'Retour à la connexion'
+                                        : 'Pas encore membre ? Inscrivez-vous ici'}
+                                </Button>
+                            </Box>
+                        </Box>
+                    </Container>
+                </Box>
+            )}
+        </>
+    )
+};    
